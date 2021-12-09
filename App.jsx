03b7d72e86443aca333cf78b3722d6e3777eff09
Luciana Gasparini Moreira Wendling <89037317+LucianaGasparini@users.noreply.github.com>
2021-12-09 18:26:22 -0300

    React native sucesso

---
+++ App.jsx
@@ -0,0 +1,26 @@
+import {NativeBaseProvider}from "native-base";
+import React from "react";
+import { StatusBar } from 'react-native';
+import "react-native-gesture-handler";
+import Menu from "./components/Menu";
+import { UsuarioProvider } from "./context";
+//import { Routes } from "./routes";
+
+
+export default function App() {
+  
+  return (
+    <UsuarioProvider>
+    <NativeBaseProvider>   
+        <Menu/>  
+      <StatusBar
+        backgroundColor="blue"       
+        barStyle="dark-content"
+      />
+      </NativeBaseProvider>
+      </UsuarioProvider>
+  );
+}
+
+
+
